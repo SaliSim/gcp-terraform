0f26787111b7701b909b0aed2d6d5f289f1f623b
SaliSim <salisimiyu319@gmail.com>
2026-02-01 11:12:29 +0300

    fmt check

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "my-bucket" {
-  name          = "cicdbuild-bucket-319"
-  location      = "US"
-  project = "spherical-depth-476520-e2"
-  force_destroy = true
+  name                     = "cicdbuild-bucket-319"
+  location                 = "US"
+  project                  = "spherical-depth-476520-e2"
+  force_destroy            = true
   public_access_prevention = "enforced"
 }
